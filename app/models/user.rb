d525ad676807fb7c77d0d479d24ba4a3f5c64b02
W Ryan Lee <ryanwonlee@gmail.com>
2013-06-28 00:17:36 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible(:email, :name, :password, :password_confirmation)
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -22,5 +22,7 @@ class User < ActiveRecord::Base
   	uniqueness: { case_sensitive: false })
   validates(:password, presence: true, length: { minimum: 6 })
   validates :password_confirmation, presence: true
+
+  after_validation { self.errors.messages.delete(:password_digest) }
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name	"Ryan Lee"
+		email	"ryanwonlee@gmail.com"
+		password	"foobar"
+		password_confirmation	"foobar"
+	end
+end
