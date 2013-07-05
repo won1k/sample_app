3648c1cc39b275f8f03ad4ad80be03321d00408c
W Ryan Lee <ryanwonlee@gmail.com>
2013-07-04 20:32:09 -0400

    Admin to integer change done for PG

--- db/migrate/20130704235929_change_admin_to_int.rb
+++ db/migrate/20130704235929_change_admin_to_int.rb
@@ -1,13 +1,10 @@
 class ChangeAdminToInt < ActiveRecord::Migration
   def up
-  	change_table :users do |user|
-  		user.change :admin, :integer
-  	end
-  end
+  	rename_column :users, :admin, :admin_boolean
+    add_column :users, :admin, :integer
 
-  def down
-  	change_table :users do |user|
-  		user.change :admin, :boolean
-  	end
+    User.reset_column_information
+    User.find_each { |user| user.update_attribute(:admin, user.admin_boolean) }
+    remove_column :users, :admin_boolean
   end
 end
