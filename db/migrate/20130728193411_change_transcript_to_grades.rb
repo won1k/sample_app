d196df0f1eb0f36bfc66f223102a9cd9d2ceb93e
W Ryan Lee <ryanwonlee@gmail.com>
2013-07-28 18:23:41 -0400

    Again DB stuff for Heroku

--- db/migrate/20130728193411_change_transcript_to_grades.rb
+++ db/migrate/20130728193411_change_transcript_to_grades.rb
@@ -1,6 +1,6 @@
 class ChangeTranscriptToGrades < ActiveRecord::Migration
   def up
-  	rename_column :users, :courses, :courses_old
+  	# rename_column :users, :courses, :courses_old
   	rename_column :users, :transcript, :transcript_old
     add_column :users, :grade1, :integer
     add_column :users, :grade2, :integer
@@ -8,9 +8,9 @@ class ChangeTranscriptToGrades < ActiveRecord::Migration
     add_column :users, :grade4, :integer
 
     User.reset_column_information
-    User.find_each { |user| user.update_attribute(:course1, user.courses_old) }
+    # User.find_each { |user| user.update_attribute(:course1, user.courses_old) }
     User.find_each { |user| user.update_attribute(:grade1, user.transcript_old) }
-    remove_column :users, :courses_old
+    # remove_column :users, :courses_old
     remove_column :users, :transcript_old
   end
 end
